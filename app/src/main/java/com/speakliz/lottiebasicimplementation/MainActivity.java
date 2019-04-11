e6fcac7c81785f8a59326b1215d2d0e8811fa922
Juan Chango <jchango@udlanet.ec>
2019-04-11 16:06:28 -0500

    Initial commit

---
+++ app/src/main/java/com/speakliz/lottiebasicimplementation/MainActivity.java
@@ -0,0 +1,19 @@
+package com.speakliz.lottiebasicimplementation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Lottie simple implementation, working!
+ * Android X Compatibility Required!
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
